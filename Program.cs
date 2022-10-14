b9c217416464670607bce0ced7f5ba2fa7f2bf89
Kasper Kristensen <kkrist87@gmail.com>
2022-10-14 17:02:46 +0200

    Installed SadConsole

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ namespace SadConsole{
     class Program{
         static void Main(string[] args){
             System.Console.WriteLine("Hello, World!");
-            Entities.Entity player;
-
-            
         }
     }
     // See https://aka.ms/new-console-template for more information   
