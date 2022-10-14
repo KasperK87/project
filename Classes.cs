7a0c4de81befb682d2de65f3d64c1adb2ea2842c
Kasper Kristensen <kkrist87@gmail.com>
2022-10-14 19:58:02 +0200

    Break

---
+++ Classes.cs
@@ -0,0 +1,23 @@
+namespace Classes
+{
+    class Game{
+        SadConsole.Console currentScreen;
+
+        public Game(SadConsole.Console console){
+            currentScreen = console;   
+        }
+
+        void update(){
+
+        }
+
+        void draw(){
+
+        }
+
+    }
+
+    class Entity{
+
+    }   
+}
