416dcf5b2aac54a035873780acbd9906aeb11f7a
Kasper Kristensen <kkrist87@gmail.com>
2022-10-14 17:00:14 +0200

    Implementing Sadconsole

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//using System;
+using SadConsole;
+
+namespace SadConsole{
+    class Program{
+        static void Main(string[] args){
+            System.Console.WriteLine("Hello, World!");
+            Entities.Entity player;
+
+            
+        }
+    }
+    // See https://aka.ms/new-console-template for more information   
+
+
+}
