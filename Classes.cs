0468dfa0443d9ddf11ea3dfc2457c2103c12cd1f
Kasper K <kakr@iba.dk>
2022-10-16 18:52:01 +0200

    Cleaned up file structure

--- Classes.cs
+++ Classes.cs
@@ -5,43 +5,6 @@ using Console = SadConsole.Console;
 
 namespace ResidentSurvivor
 {
-    class Game : SadConsole.Game{
-        // Managers
-       public static UIManager? UIManager;
-       private Game(){
-            //gameInstance is a singleton use setup(width, height)
-       } 
-
-       public static void Setup(int w, int h){
-            Create(w, h);
-            Instance.OnStart = () => {
-                UIManager = new UIManager();
-
-                Game.Instance.Screen = UIManager;
-                Game.Instance.DestroyDefaultStartingConsole();
-            };
-
-            // Hook the update event that happens each frame so we can trap keys and respond.
-            SadConsole.Game.Instance.FrameUpdate += Update;
-       }
-
-       // allowing some game logic during frame updates
-        private static void Update(object? sender, GameHost? host)
-        {
-            // Called each logic update
-            // As an example, we'll use the F5 key to make the game full screen
-            if (SadConsole.Game.Instance.Keyboard.IsKeyReleased(SadConsole.Input.Keys.F5)){
-                SadConsole.Game.Instance.ToggleFullScreen();
-            }
-
-            if (SadConsole.Game.Instance.Keyboard.IsKeyReleased(SadConsole.Input.Keys.Escape)){
-                //SadConsole.Game.Instance.Dispose();
-                SadConsole.Game.Instance.MonoGameInstance.Exit();
-            }
-            
-        }
-    }
-
      public class UIManager : ScreenObject{
      // Creates/Holds/Destroys all consoles used in the game
      // and makes consoles easily addressable from a central place.
@@ -118,8 +81,8 @@ namespace ResidentSurvivor
             this.View = new Rectangle(0, 0, 22, 22);
             this.CanDrag = true;
             this.Title = "MENU";
+            
             //this.Children.Add(menuConsole);    
-
             
             //Add close button to the Window's list of UI elements
             SadConsole.UI.Controls.Button closeButton = new SadConsole.UI.Controls.Button(3, 1);
@@ -147,13 +110,13 @@ namespace ResidentSurvivor
             this.Controls.Add(startButton);
 
             //WHY NOT WORKING?
-            this.DefaultBackground = Color.Red;
-            this.DefaultForeground = Color.Orange;
+            this.DefaultBackground = Color.Blue;
+            this.DefaultForeground = Color.White;
 
             //WORK AROUND
             for (int i = 0; i < this.Width; i++)
                 for (int j = 0; j < this.Height; j++)
-                    this.SetBackground(i,j, Color.Red);
+                    this.SetBackground(i,j, Color.Blue);
 
             this.Show();
         }
@@ -163,145 +126,6 @@ namespace ResidentSurvivor
             base.Render(delta);
 
             //menuConsole.Print(1, 1, "MENU");
-        }
-    }
-
-    class World : Console {
-        private static SadConsole.Entities.Entity player;
-
-        public RogueSharpSadConsoleSamples.Core.DungeonMap DungeonMap;
-
-        private RogueSharp.IMap _map;
-
-        private TimeSpan timer;
-        private bool preKeyDown; 
-        public static RogueSharp.Random.IRandom Random { get; private set; }
-
-
-        //int width, int height, int maxRooms, int roomMaxSize, int roomMinSize, int level
-        RogueSharpSadConsoleSamples.Systems.MapGenerator
-            mapGenerator = new RogueSharpSadConsoleSamples.Systems.MapGenerator(
-                120,40, 10, 10, 5, 1);
-
-        private SadConsole.Entities.Renderer entityManager;
-        public World(int w, int h) : base( w, h){
-            entityManager = new SadConsole.Entities.Renderer();
-
-            timer = TimeSpan.Zero;
-            preKeyDown = false;
-
-            SadComponents.Add(entityManager);
-
-            CreatePlayer();
-
-            entityManager.Add(player);
-
-            // Setup this console to accept keyboard input.
-            //UseKeyboard = true;
-            IsVisible = true;
-
-            //create dungeon
-            //DungeonMap = mapGenerator.CreateMap();
-            int seed = (int) DateTime.UtcNow.Ticks;
-            Random = new RogueSharp.Random.DotNetRandom( seed );
-
-            RogueSharp.MapCreation.IMapCreationStrategy<RogueSharpSadConsoleSamples.Core.DungeonMap> mapCreationStrategy =
-                new RogueSharp.MapCreation.RandomRoomsMapCreationStrategy<RogueSharpSadConsoleSamples.Core.DungeonMap>( 50, 30, 100, 7, 3 );
-            DungeonMap = mapCreationStrategy.CreateMap();
-
-            
-            //DungeonMap = new RogueSharp.MapCreation.RandomRoomsMapCreationStrategy(120, 40, 20, 30, 15, Random);
-        }
-
-        // Create a player using SadConsole's Entity class
-        private static void CreatePlayer()
-        {
-            player = new SadConsole.Entities.Entity(
-                Color.White, Color.Black, 1, 100);
-            player.Position = new Point(5,5);
-        }
-
-        public override void Update(TimeSpan delta){
-            timer += delta;
-
-            ProcessKeyboard(SadConsole.Game.Instance.GetKeyboardState());
-              
-            DungeonMap.UpdatePlayerFieldOfView(player);
-        }
-
-        public override void Render(TimeSpan delta){
-            base.Render(delta);
-
-            DungeonMap.Draw(this);
-
-        }
-    
-        public bool ProcessKeyboard(SadConsole.Input.IKeyboardState info)
-        {
-            // Forward the keyboard data to the entity to handle the movement code.
-            // We could detect if the users hit ESC and popup a menu or something.
-            // By not setting the entity as the active object, twe let this
-            // "game level" (the console we're hosting the entity on) determine if
-            // the keyboard data should be sent to the entity.
-
-            // Process logic for moving the entity.
-            bool keyHit = false;
-            bool run = false;
-
-            Point oldPosition = player.Position;
-            Point newPosition = (0, 0);
-
-            // Process UP/DOWN movements
-            if (info.IsKeyDown(SadConsole.Input.Keys.Up))
-            {
-                newPosition = player.Position + (0, -1);
-                keyHit = true;
-            }
-            else if (info.IsKeyDown(SadConsole.Input.Keys.Down))
-            {
-                newPosition = player.Position + (0, 1);
-                keyHit = true;
-            }
-
-            // Process LEFT/RIGHT movements
-            if (info.IsKeyDown(SadConsole.Input.Keys.Left))
-            {
-                newPosition = player.Position + (-1, 0);
-                keyHit = true;
-            }
-            else if (info.IsKeyDown(SadConsole.Input.Keys.Right))
-            {
-                newPosition = player.Position + (1, 0);
-                keyHit = true;
-            }
-
-            if(preKeyDown && keyHit && timer >= TimeSpan.FromMilliseconds(500)){
-                run = true;
-            } else if (!preKeyDown && !keyHit) {
-                timer = TimeSpan.Zero;
-            }
-
-            // If a movement key was pressed
-            if ((keyHit && !preKeyDown) || run)
-            {
-                // Check if the new position is valid
-                if (Surface.Area.Contains(newPosition))
-                {
-                    // Entity moved. Let's draw a trail of where they moved from.
-                    Surface.SetGlyph(player.Position.X, player.Position.Y, 250);
-                    player.Position = newPosition;
-
-                    preKeyDown = keyHit;
-                    return true;
-                }
-            }
-
-            // You could have multiple entities in the game for example, and change
-            // which entity gets keyboard commands.
-
-        
-            preKeyDown = keyHit;
-            return false;
         }
     }
 }
---
+++ Game.cs
@@ -0,0 +1,44 @@
+using SadConsole;
+using SadConsole.UI;
+using SadRogue.Primitives;
+using Console = SadConsole.Console;
+
+namespace ResidentSurvivor
+{
+    class Game : SadConsole.Game{
+        // Managers
+       public static UIManager? UIManager;
+       private Game(){
+            //gameInstance is a singleton use setup(width, height)
+       } 
+
+       public static void Setup(int w, int h){
+            Create(w, h);
+            Instance.OnStart = () => {
+                UIManager = new UIManager();
+
+                Game.Instance.Screen = UIManager;
+                Game.Instance.DestroyDefaultStartingConsole();
+            };
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Game.Instance.FrameUpdate += Update;
+       }
+
+       // allowing some game logic during frame updates
+        private static void Update(object? sender, GameHost? host)
+        {
+            // Called each logic update
+            // As an example, we'll use the F5 key to make the game full screen
+            if (SadConsole.Game.Instance.Keyboard.IsKeyReleased(SadConsole.Input.Keys.F5)){
+                SadConsole.Game.Instance.ToggleFullScreen();
+            }
+
+            if (SadConsole.Game.Instance.Keyboard.IsKeyReleased(SadConsole.Input.Keys.Escape)){
+                //SadConsole.Game.Instance.Dispose();
+                SadConsole.Game.Instance.MonoGameInstance.Exit();
+            }
+            
+        }
+    }
+}
