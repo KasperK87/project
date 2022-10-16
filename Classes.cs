cd77f62c60e464c636e182b6a0a68b9604f58ed5
Kasper K <kakr@iba.dk>
2022-10-16 22:30:18 +0200

    Improved splash screen

--- Classes.cs
+++ Classes.cs
@@ -52,19 +52,27 @@ namespace ResidentSurvivor
 
             timer += delta;
 
+
+
             if(timer >= TimeSpan.FromSeconds(3)){
-                Game.Instance.Screen.Children.Add(new Menu(22,22));
+                Menu menu = new Menu(120,40);
+                
+                Game.Instance.Screen.Children.Add(menu);
+
                 Game.Instance.Screen.Children.Remove(this);
             }
         }
 
         public override void Render(TimeSpan delta)
         {
-            base.Render(delta);
-            int offcentering = 15;
-            Surface.PrintTheDraw(2+offcentering, this.Height/2-10, "Resident", _selectedFont);
-            Surface.PrintTheDraw(this.Width-80-offcentering, this.Height/2, "Survivor", _selectedFont);
+            this.Clear();
 
+            int offcenteringX = 15;
+            int offcenteringY = (int)(timer.TotalMilliseconds/300);
+            Surface.PrintTheDraw(2+offcenteringX, this.Height/2-10-offcenteringY, "Resident", _selectedFont);
+            Surface.PrintTheDraw(this.Width-80-offcenteringX, this.Height/2-offcenteringY, "Survivor", _selectedFont);
+
+            base.Render(delta);
             //this.Print(1, 1, "Resident Survivor");
         }
     }
@@ -72,18 +80,20 @@ namespace ResidentSurvivor
     class Menu : SadConsole.UI.Window{
         //private Console menuConsole;
         
+        private SadConsole.Readers.TheDrawFont[] fonts;
+        private SadConsole.Readers.TheDrawFont _selectedFont;
+
         public Menu(int w, int h) : base(w,h){
 
-            //menuConsole = new Console(w-2,h-2);
-            //menuConsole.Position = new Point(1,1);
-            //menuConsole.DefaultBackground = Color.Red;
+            fonts = SadConsole.Readers.TheDrawFont.ReadFonts("./fonts/TheDraw/ABBADON.TDF").ToArray();
+            _selectedFont = fonts[0];
 
-            this.View = new Rectangle(0, 0, 22, 22);
-            this.CanDrag = true;
-            this.Title = "MENU";
+            //this.View = new Rectangle(0, 0, 22, 22);
+           //this.CanDrag = true;
+            this.Title = "MAIN MENU";
             
             //this.Children.Add(menuConsole);    
-            
+            /*
             //Add close button to the Window's list of UI elements
             SadConsole.UI.Controls.Button closeButton = new SadConsole.UI.Controls.Button(3, 1);
             closeButton.Position = new Point(0, 0);
@@ -95,18 +105,21 @@ namespace ResidentSurvivor
                 this.Parent.Children.Remove(this);
                 //this.Hide();
             };
+            */
 
             //adding a start button
             SadConsole.UI.Controls.Button startButton = new SadConsole.UI.Controls.Button(7,1);
+            startButton.Theme = new SadConsole.UI.Themes.ButtonTheme();
+            //startButton.SetThemeColors();
             startButton.Position = new Point(7,10);
             startButton.Text = "START";
             startButton.Click += (x, y) => {
                 World newWorld = new World(118,38);
                 newWorld.Position = new Point(1,1);
                 newWorld.DefaultBackground = Color.Black;
-                Game.Instance.Screen.Children.Add(newWorld);
-                
+                Game.Instance.Screen.Children.Add(newWorld);  
             };
+            
             this.Controls.Add(startButton);
 
             //WHY NOT WORKING?
@@ -115,15 +128,24 @@ namespace ResidentSurvivor
 
             //WORK AROUND
             for (int i = 0; i < this.Width; i++)
-                for (int j = 0; j < this.Height; j++)
-                    this.SetBackground(i,j, Color.Blue);
+                for (int j = 0; j < this.Height; j++){
+                    this.SetBackground(i,j, Color.Black);
+                    this.SetForeground(i,j, Color.Orange);
+                }
 
             this.Show();
+
+            //AbsolutePosition = new Point(
+            //    500, 100);
         }
 
         public override void Render(TimeSpan delta)
-        {
+        {            
             base.Render(delta);
+            int offcentering = 15;
+            Surface.PrintTheDraw(2+offcentering, this.Height/2-19, "Resident", _selectedFont);
+            Surface.PrintTheDraw(this.Width-80-offcentering, this.Height/2-9, "Survivor", _selectedFont);
+
 
             //menuConsole.Print(1, 1, "MENU");
         }
