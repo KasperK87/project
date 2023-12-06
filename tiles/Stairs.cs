8c15b48cc81eda469227c7b03fa642885f338449
Kasper Kristensen <kkrist87@gmail.com>
2023-12-06 15:11:48 +0100

    Fixed stairs changing levels rapidly

--- tiles/Stairs.cs
+++ tiles/Stairs.cs
@@ -4,6 +4,10 @@ namespace ResidentSurvivor
     {
         private bool IsUp;
 
+        //Used to prevent spamming of stairs
+        //across levels
+        static TimeSpan timer = TimeSpan.Zero;
+
         public Stairs(SadRogue.Primitives.Color c1,SadRogue.Primitives.Color c2, int SetGlyph, int zIndex, bool setIsUp):
             base(c1, c2, SetGlyph, zIndex){
                 this.Name = "Stairs";
@@ -15,6 +19,7 @@ namespace ResidentSurvivor
         }
 
         public override void Update(TimeSpan delta){
+            timer += delta;
             base.Update(delta);
         }
 
@@ -22,11 +27,13 @@ namespace ResidentSurvivor
             //Mock up code for stairs
             Game.Instance.Screen.Children.Remove(Game.UIManager.dungeon.getCurrentFloor());
 
-            if(!IsUp){
+            if(!IsUp && TimeSpan.FromMilliseconds(500) < timer){
                 Game.UIManager.dungeon.setLevel(Game.UIManager.dungeon.getCurrentLevel()+1);    
-            } else {
+            } else if (IsUp && TimeSpan.FromMilliseconds(500) < timer){
                 Game.UIManager.dungeon.setLevel(Game.UIManager.dungeon.getCurrentLevel()-1); 
             }
+
+            timer = TimeSpan.Zero;
 
             Game.UIManager.currentFloor = Game.UIManager.dungeon.getCurrentFloor();
             Game.UIManager.Children.Add(Game.UIManager.currentFloor);
