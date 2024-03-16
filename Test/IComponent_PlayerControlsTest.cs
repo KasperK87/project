f2efb8c2aa74d7fce1284edbb8006b0069b2f8f7
Kasper Kristensen (KSKR - Lektor - SK - SESG) <kskr@sesg.dk>
2024-03-16 09:36:04 +0100

    Added xunit to project.
    Best practice is to have unit tests in a seperated project. But we a little loosy goosy here.

---
+++ Test/IComponent_PlayerControlsTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace ResidentSurvivorTest
+{
+    public class IComponent_PlayerControlsTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            //Arrange
+            int i = 0;
+            //Act
+            //i = 1;
+            //Assert
+            Assert.Equal(0, i);
+        }
+    }
+}
